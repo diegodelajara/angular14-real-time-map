2347b104ed941283b92bb8ae8eebc74e4cd2745c
Diego de la Jara <d.delajaral@gmail.com>
2025-01-08 17:39:56 -0300

    feat: :sparkles: Add google maps + socket on angular14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { log } from 'console';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular14-real-time-app';
+  address: string = '';
+  lat: number = -33.4791745;
+  lng: number = -70.6004407;
+  zoom: number = 8;
+  center: google.maps.LatLngLiteral = { lat: this.lat, lng: this.lng };
+  markers: { lat: number; lng: number }[] = []; // Lista de marcadores
+
+
+  constructor(private socket: Socket) {}
+  ngOnInit() {
+
+    this.socket.on('newLocation', (data: { lat: number; lng: number }) => {
+      this.addMarker(-33.4791745, -70.6004407);
+    console.log('markers', this.markers)
+    });
+  }
+
+  searchAddress() {
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: this.address }, (results, status) => {
+      if (status === 'OK') {
+        this.lat = results && results[0].geometry.location.lat() || 0;
+        this.lng = results && results[0].geometry.location.lng() || 0;
+        this.center = { lat: this.lat, lng: this.lng };
+        this.zoom = 13;
+
+        this.socket.emit('newLocation', { lat: this.lat, lng: this.lng });
+        this.addMarker(this.lat, this.lng);
+      } else {
+        alert('No se pudo encontrar la dirección: ' + status);
+      }
+    });
+  }
+  addMarker(lat: number, lng: number) {
+    this.markers.push({ lat, lng });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { FormsModule } from '@angular/forms';  // Importa FormsModule
+
+
+
+const config: SocketIoConfig = { url: 'https://stage.allrideapp.com/tech_interview', options: {
+  query: { room: 'diegodelajara'},
+  transports: ['websocket']
+} };
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, SocketIoModule.forRoot(config), GoogleMapsModule, FormsModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
